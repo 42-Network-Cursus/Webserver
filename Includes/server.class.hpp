928e8d964dc7690a3503da0c0e2fae6adb4f19c1
Constantin Wastchenko <cwastche@student.s19.be>
2023-06-21 11:09:56 +0200

    Removed Configuration class, useless

--- Includes/server.class.hpp
+++ Includes/server.class.hpp
@@ -3,6 +3,32 @@
 
 # include "std_libs.hpp"
 
+
+// std::string Configuration::getBody(int fd) {
+// 	std::vector<Server>::iterator it_begin = _server_list.begin();
+// 	std::vector<Server>::iterator it_end = _server_list.end();
+
+// 	for (; it_begin != it_end; it_begin++) {
+// 		if (it_begin->sockfd == fd)
+// 			break;
+// 	}
+	
+// 	std::string body;
+// 	std::ifstream file_stream (it_begin->root + it_begin->index);
+
+// 	if (!file_stream.is_open()) { // check whether the file is open
+// 		std::cout << "Error reading conf file" << std::endl;
+// 		exit(1);
+// 	}
+	
+// 	body.assign ( 	(std::istreambuf_iterator<char>(file_stream)),
+// 					(std::istreambuf_iterator<char>()) 
+// 				);
+
+// 	return body;
+// }
+
+
 class Server {
 	public:
 		Server();
