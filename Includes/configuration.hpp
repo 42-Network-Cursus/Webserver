cd375dda7e5335cfff8bfc2ad3970de8eca91eee
cwastche <cwastche@student.s19.be>
2023-06-29 15:26:44 +0200

    WIp

--- Includes/configuration.hpp
+++ Includes/configuration.hpp
@@ -13,6 +13,22 @@ enum conf_param {
 };
 
 conf_param 			resolve_conf_param(std::string param);
-void configure_servers(int argc, char *argv[], std::vector<Server *> *servers);
+void 				configure_servers(int argc, char *argv[], std::vector<Server *> *servers);
 
 #endif // CONFIGURATION_HPP
+
+/*
+	To do
+
+		Location class -> vector
+		redo parsing of conf file
+
+		think of server_name and how to use in rest of code
+
+		Wtf happens if same host:port for 2 servers ?
+			-> ask chat
+
+		conf checker -> if no useable server, break shit
+		
+
+*/ 
