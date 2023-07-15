937c89e1d9f5a4ec10dc1a7ba16b7f30e25b4713
Skialogia <loiccena@live.be>
2023-07-15 17:07:51 +0200

    Ajout de l'autoindex --> A revoir avec Constantin

---
+++ Srcs/autoIndex.cpp
@@ -0,0 +1,38 @@
+#include "autoIndex.hpp"
+
+/**
+ * @brief Generate the HTML Code oh an autoIndex page
+ * 
+ * @param path Path of the directory
+ * @param host the Host ? (Not directly merge with port ?)
+ * @param port the port ? (Not directly merge with host ?)
+ * @return std::string empty if can't open the directory, or the html page in std::string
+ */
+std::string generateAutoIndex(std::string path, std::string host, std::string port)
+{
+	DIR *dir = opendir(path.c_str());
+	if (dir == NULL)
+	{
+		std::cout << "Error Open directory in generate AutoIndex" << std::endl;
+		return "";
+	}
+	if (path[0] != '/')
+		path = "/" + path;
+	std::string page = "<!DOCTYPE html><html><head><title>" + path + "</title></head><body>";
+
+	struct dirent *entry;
+	entry = readdir(dir);
+
+	std::string name;
+	while (entry != NULL)
+	{
+		name = std::string(entry->d_name);
+		page += "<p><a href=\"http://" + host + ":" + port + path + "/" + name + "\">" + name + "</a></p>";
+		entry = readdir(dir);
+	}
+
+	page += "</body></html>";
+	closedir(dir);
+
+	return (page);
+}
